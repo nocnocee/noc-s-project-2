227e0308f170ac1ffad6148c8b2cf00aedd822a9
nocnoc_e <dhgustmd25@hotmail.com>
2023-02-07 02:46:59 -0500

    random quote works

--- controllers/dailyqController.js
+++ controllers/dailyqController.js
@@ -2,6 +2,7 @@
 //// Import Dependencies         ////
 /////////////////////////////////////
 const express = require('express')
+const { count } = require('../models/comment')
 const Dailyq = require('../models/dailyqs')
 
 
@@ -9,12 +10,15 @@ const Dailyq = require('../models/dailyqs')
 //// Create Router               ////
 /////////////////////////////////////
 const router = express.Router()
+const limit = 10
 
 //////////////////////////////
 //// Routes               ////
 //////////////////////////////
 router.get('/', (req, res) => {
-    Dailyq.find({})
+    let random = Math.floor(Math.random() * limit) % limit
+    console.log(random)
+    Dailyq.findOne().skip(random)
 
         // send json if successful
         .then(dailyqs => { res.json({ dailyqs: dailyqs })})
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require('dotenv').config() // Load my ENV file's variables
 const path = require('path') // import path module
 const UserRouter = require('./controllers/userControllers')
 const CommentRouter = require('./controllers/commentControllers')
+const DailyqRouter = require('./controllers/dailyqController')
 const middleware = require('./utils/middleware')
 
 /////////////////////////////////////
@@ -17,9 +18,6 @@ const app = express()
 /////////////////////////////////////
 //// Middleware                  ////
 /////////////////////////////////////
-// middleware runs before all the routes.
-// every request is processed through our middleware before mongoose can do anything with it
-// our middleware is now processed by a function in the utils directory. This middleware function takes one argument, app, and processes requests through our middleware
 middleware(app)
 
 
@@ -30,11 +28,10 @@ app.get('/', (req, res) => {
     res.send('Server is live, ready for requests')
 })
 
-// This is now where we register our routes, this is how server.js knows to send the correc response. 
-// app.use, when we register a route, needs two arguments
-// the first arg is the base URL, second arg is the file to use.
+
 app.use('/comments', CommentRouter)
 app.use('/users', UserRouter)
+app.use('/dailyqs', DailyqRouter)
 
 /////////////////////////////////////
 //// Server Listener             ////
